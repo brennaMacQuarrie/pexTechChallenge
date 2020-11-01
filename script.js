a14bb0efcb60124a84d21f8fdb754a1a70832289
brennaMacQuarrie <brennamacqurrie@gmail.com>
2020-11-01 08:03:56 -0700

    ms display

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // NAMESPACE
 const happy = {};
 
-// happy.url = 'https://bit.ly/3mOKGpu';
+// TODO MAKE DISPLAY DYNAMIC IN REGARDS TO WHAT SONGS ARE HAPPIEST??
+
+happy.url = 'https://spreadsheets.google.com/feeds/list/1H5S6Vc-gCOCKLvQmfjfJmG2THtDb5Z_LQGaZJpWZQ4c/1/public/values?alt=json';
 
 
 // TODO make function to get heart and ellipsis to appear on song hover
@@ -17,13 +19,21 @@ happy.show = () => {
     }
 }
 
+
 // takes in data // returns individual song row
 happy.buildSong = (songData) => {
         let songTitle = songData.gsx$songtitle.$t;
         let songArtist = songData.gsx$artist.$t;
-        let runTime = (songData.gsx$durationms.$t / 6000).toFixed(1); 
-        // TODO: DISPLAY as TIME with : not .
-        let minTime = runTime / Math.pow(10, 1);
+
+        // TODO seconds to ms build external func?
+        var ms = songData.gsx$durationms.$t;
+        ms = 1000 * Math.round(ms / 1000); // round to nearest second
+        var d = new Date(ms);
+        d = d.toTimeString();
+        d = d.split(' ')[0]; // access hr:mn:sc
+        let mins = parseInt(d.split(':')[1]); // remove 0
+        let secs = d.split(':')[2]; // remove hr
+        let minTime = mins + ':' + secs; // concat result
         
 
         const template = document.getElementById('songRow');
@@ -33,16 +43,17 @@ happy.buildSong = (songData) => {
         
         songRow.querySelector('.songTitle').textContent = songTitle;
         songRow.querySelector('.songArtist').textContent = songArtist;
-        songRow.querySelector('.runTime').textContent = minTime.toFixed(2);
+        songRow.querySelector('.runTime').textContent = minTime;
 
         return songRow;
 
 }
 
+
 happy.populateList = () => {
     //get data from bitly json info
     const apiCall = function () {
-        fetch('https://spreadsheets.google.com/feeds/list/1H5S6Vc-gCOCKLvQmfjfJmG2THtDb5Z_LQGaZJpWZQ4c/1/public/values?alt=json')
+        fetch(happy.url)
             .then(function (response) {
                 if (response.status !== 200) {
                     console.log(
@@ -55,22 +66,25 @@ happy.populateList = () => {
                     console.log(data);
                     const items = data.feed.entry;
 
+                    // TODO The contents of the playlist should be 100 songs listed from “happiest” to “least happy”.
                     let songs = [];
 
                     items.forEach((songData) => {
+                        // var holds each func call /temp build per item
                         let songRow = happy.buildSong(songData);
+                        // send each build template into the array
                         songs.push(songRow);
-                        // console.log(songRow);
+                        // build them into the songList one by one
                         document.getElementById('songList').append(songRow);
+                        
                     });
-                    // console.log(songs);
                 });
             })
+            // if shit don't work
             .catch(function (err) {
-                alert('data error');
+                alert('data error:', err);
             });
     }
-
     apiCall();
 }
 
